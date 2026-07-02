e747300529895ce651d38e2a37c37864942365a5
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2026-07-02 14:13:17 +0300

    updated subscription form

--- src/js/components/subscription.js
+++ src/js/components/subscription.js
@@ -1,3 +1,41 @@
-export function initSubscription() {
-  // TODO: Validate email and call yourEnergyApi.subscribe().
+import { yourEnergyApi } from '../api/your-energy-api.js';
+import { beginButtonLoader } from './loader.js';
+import iziToast from './notifications.js';
+
+const refs = {
+  subscribeForm: document.querySelector('[data-subscribe-form]'),
+};
+
+async function onSubscription(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const emailValue = form.elements.email.value.trim();
+
+  const button = form.querySelector('.subscription-form__button');
+  const loading = beginButtonLoader(button);
+
+  try {
+    await yourEnergyApi.subscribe(emailValue);
+
+    iziToast.show({
+      backgroundColor: '#59C991',
+      message: '✅ Thank you for subscribing',
+    });
+
+    form.reset();
+  } catch (e) {
+    iziToast.show({
+      backgroundColor: '#E26353',
+      message: 'Subscription already exist',
+    });
+  } finally {
+    await loading.stop();
+  }
+}
+
+export function initSubscriptionForms() {
+  if (refs.subscribeForm) {
+    refs.subscribeForm.addEventListener('submit', onSubscription);
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,8 @@ import { initQuote } from './components/quote.js';
 import { initExercises } from './components/exercises.js';
 import { initExerciseModal } from './components/exercise-modal.js';
 import { initRatingModal } from './components/rating-modal.js';
-import { initSubscription } from './components/subscription.js';
+import { initSubscriptionForms } from './components/subscription.js';
 import { initLoader } from './components/loader.js';
-import { initNotifications } from './components/notifications.js';
 import { initScrollUp } from './components/scroll-up.js';
 
 function initHomePage() {
@@ -16,9 +15,8 @@ function initHomePage() {
   initExercises();
   initExerciseModal();
   initRatingModal();
-  initSubscription();
+  initSubscriptionForms();
   initLoader();
-  initNotifications();
   initScrollUp();
 }
 
